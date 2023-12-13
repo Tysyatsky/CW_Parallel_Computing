fbf62cda6cc2886b2d7fc992a40672594400c661
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-12-14 01:29:45 +0200

    feat: adding words from docs to the inverted index

--- CourseWork/Program.cs
+++ CourseWork/Program.cs
@@ -10,18 +10,17 @@
         static void Main(string[] args)
         {
             InitialTest();
+            FilesTest();
         }
 
         static void InitialTest()
         {
             InvertedIndex invertedIndex = new();
 
-            // Add documents to the index
             invertedIndex.AddDocument(1, doc1);
             invertedIndex.AddDocument(2, doc2);
             invertedIndex.AddDocument(3, doc3);
 
-            // Search for documents containing a term
             string searchTerm = "banana";
             List<int> result = invertedIndex.Search(searchTerm);
 
@@ -31,14 +30,51 @@
                 Console.WriteLine($"Document {docId}");
             }
 
-            // Print the inverted index
             Console.WriteLine("\nInverted Index:");
             invertedIndex.PrintIndex();
         }
 
-        static void SearchEngineTest()
+        static void FilesTest()
         {
+            InvertedIndex invertedIndex = new();
 
+            FilesFinder(invertedIndex);
+
+            var searchTerm = "word";
+
+            // Too many words
+            // invertedIndex.PrintIndex();
+
+            List<int> result = invertedIndex.Search(searchTerm);
+
+            Console.WriteLine($"Documents containing '{searchTerm}':");
+            foreach (var docId in result)
+            {
+                Console.WriteLine($"Document {docId}");
+            }
+        }
+
+        static void FilesFinder(InvertedIndex invertedIndex)
+        {
+            string directoryPath = @"C:\Users\tysya\Desktop\test_n";
+
+            try
+            {
+                string[] filePaths = Directory.GetFiles(directoryPath);
+
+                foreach (string filePath in filePaths)
+                {
+                    var words = FileReader.ReadWordsFromFile(filePath);
+
+                    _ = int.TryParse(Path.GetFileName(filePath).Split('_')[0], out int localDocId);
+
+                    invertedIndex.AddDocument(localDocId, words);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
