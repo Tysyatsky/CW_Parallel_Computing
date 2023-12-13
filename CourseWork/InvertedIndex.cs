913e412671fa9f4e9bc554979de36d62ef03a12b
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-12-14 00:51:30 +0200

    feat: Inverted indexing simple solution with testing

---
+++ CourseWork/InvertedIndex.cs
@@ -0,0 +1,53 @@
+﻿namespace CourseWork
+{
+    class InvertedIndex
+    {
+        private Dictionary<string, List<int>> index;
+
+        public InvertedIndex()
+        {
+            index = new Dictionary<string, List<int>>();
+        }
+
+        public void AddDocument(int documentId, string[] terms)
+        {
+            foreach (var term in terms)
+            {
+                if (!index.ContainsKey(term))
+                {
+                    index[term] = new List<int>();
+                }
+
+                if (!index[term].Contains(documentId))
+                {
+                    index[term].Add(documentId);
+                }
+            }
+        }
+
+        public List<int> Search(string term)
+        {
+            if (index.ContainsKey(term))
+            {
+                return index[term];
+            }
+            else
+            {
+                return new List<int>();
+            }
+        }
+
+        public void PrintIndex()
+        {
+            foreach (var term in index.Keys)
+            {
+                Console.Write($"{term}: ");
+                foreach (var docId in index[term])
+                {
+                    Console.Write($"{docId} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ CourseWork/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace CourseWork
+{
+    public class Program
+    {
+        // test documents
+        readonly static string[] doc1 = { "apple", "banana", "orange" };
+        readonly static string[] doc2 = { "apple", "pear", "grape" };
+        readonly static string[] doc3 = { "banana", "kiwi", "orange" };
+
+        static void Main(string[] args)
+        {
+            InitialTest();
+        }
+
+        static void InitialTest()
+        {
+            InvertedIndex invertedIndex = new();
+
+            // Add documents to the index
+            invertedIndex.AddDocument(1, doc1);
+            invertedIndex.AddDocument(2, doc2);
+            invertedIndex.AddDocument(3, doc3);
+
+            // Search for documents containing a term
+            string searchTerm = "banana";
+            List<int> result = invertedIndex.Search(searchTerm);
+
+            Console.WriteLine($"Documents containing '{searchTerm}':");
+            foreach (var docId in result)
+            {
+                Console.WriteLine($"Document {docId}");
+            }
+
+            // Print the inverted index
+            Console.WriteLine("\nInverted Index:");
+            invertedIndex.PrintIndex();
+        }
+
+        static void SearchEngineTest()
+        {
+
+        }
+    }
+}
