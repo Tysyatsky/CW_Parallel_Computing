7c9af895e90d372593ebd455ebf585e5a80c640a
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-12-14 01:45:09 +0200

    feat: client for the server

---
+++ Client/ClientMessanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+public class Client
+{
+
+    public static void SendMessage(Socket socket, string message)
+    {
+        byte[] messageBytes = Encoding.ASCII.GetBytes(message);
+        socket.Send(messageBytes);
+    }
+
+    public static string ReceiveMessage(Socket socket)
+    {
+        byte[] receivedBytes = new byte[4096];
+        int bytesRead = socket.Receive(receivedBytes);
+        return Encoding.ASCII.GetString(receivedBytes, 0, bytesRead);
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Sockets;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string serverIp = "127.0.0.1"; // Replace with the actual IP address or hostname of the server
+            int serverPort = 6000; // Replace with the port number the server is listening on
+
+            Socket clientSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                clientSocket.Connect(serverIp, serverPort);
+                Console.WriteLine("Connected to the server.");
+
+                // Send a message to the server
+                string messageToSend = "Hello, server!";
+                Client.SendMessage(clientSocket, messageToSend);
+
+                // Receive the server's response
+                string receivedMessage = Client.ReceiveMessage(clientSocket);
+                Console.WriteLine($"Server response: {receivedMessage}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                clientSocket.Close();
+            }
+        }
+    }
+}
