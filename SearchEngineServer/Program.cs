0d8cfe5c76d061af3b166ed45cca6793df6199a7
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-12-14 23:48:48 +0200

    feat: change communication with client and aaded error catching

--- SearchEngineServer/Program.cs
+++ SearchEngineServer/Program.cs
@@ -1,18 +1,89 @@
-﻿using System.Net.Sockets;
-using System.Net;
-using System.Text;
+﻿using SearchEngineData;
 
 namespace SearchEngineServer
 {
-    internal class Program
+    public class Program
     {
+        readonly static string[] doc1 = { "apple", "banana", "orange" };
+        readonly static string[] doc2 = { "apple", "pear", "grape" };
+        readonly static string[] doc3 = { "banana", "kiwi", "orange" };
+
         static void Main()
         {
+            // InitialTest();
+            // FilesTest();
+
+            InvertedIndex invertedIndex = new();
+
+            FilesFinder(invertedIndex, @"C:\Users\tysya\Desktop\test_n");
+
             int port = 6000; // Specify the port number you want the server to listen on
-            _ = new Server(port);
+            _ = new Server(port, invertedIndex);
 
             Console.WriteLine($"Server is listening on port {port}. Press any key to exit.");
             Console.ReadKey();
+        }
+
+        static void InitialTest()
+        {
+            InvertedIndex invertedIndex = new();
+
+            invertedIndex.AddDocument(1, doc1);
+            invertedIndex.AddDocument(2, doc2);
+            invertedIndex.AddDocument(3, doc3);
+
+            string searchTerm = "banana";
+            List<int> result = invertedIndex.Search(searchTerm);
+
+            Console.WriteLine($"Documents containing '{searchTerm}':");
+            foreach (var docId in result)
+            {
+                Console.WriteLine($"Document {docId}");
+            }
+
+            Console.WriteLine("\nInverted Index:");
+            invertedIndex.PrintIndex();
+        }
+
+        static void FilesTest()
+        {
+            InvertedIndex invertedIndex = new();
+
+            FilesFinder(invertedIndex, @"C:\Users\tysya\Desktop\test_n");
+
+            var searchTerm = "word";
+
+            // Too many words
+            // invertedIndex.PrintIndex();
+
+            List<int> result = invertedIndex.Search(searchTerm);
+
+            Console.WriteLine($"Documents containing '{searchTerm}':");
+            foreach (var docId in result)
+            {
+                Console.WriteLine($"Document {docId}");
+            }
+        }
+
+        static void FilesFinder(InvertedIndex invertedIndex, string directoryPath)
+        {
+            try
+            {
+                string[] filePaths = Directory.GetFiles(directoryPath);
+
+                foreach (string filePath in filePaths)
+                {
+                    var words = FileReader.ReadWordsFromFile(filePath);
+
+                    _ = int.TryParse(Path.GetFileName(filePath).Split('_')[0], out int localDocId);
+
+                    invertedIndex.AddDocument(localDocId, words);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
--- SearchEngineServer/Server.cs
+++ SearchEngineServer/Server.cs
@@ -1,15 +1,18 @@
 ﻿using System.Net.Sockets;
 using System.Net;
 using System.Text;
+using SearchEngineData;
 
 namespace SearchEngineServer;
 
 public class Server
 {
     private readonly Socket _listener;
+    private readonly InvertedIndex _invertedIndex;
 
-    public Server(int port)
+    public Server(int port, InvertedIndex invertedIndex)
     {
+        _invertedIndex = invertedIndex;
         _listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         _listener.Bind(new IPEndPoint(IPAddress.Any, port));
         _listener.Listen(10);
@@ -19,7 +22,7 @@ public class Server
         while (true)
         {
             Socket clientSocket = _listener.Accept();
-            Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+            Thread clientThread = new(new ParameterizedThreadStart(HandleClientComm));
             clientThread.Start(clientSocket);
         }
     }
@@ -27,22 +30,39 @@ public class Server
     private void HandleClientComm(object clientObj)
     {
         Socket clientSocket = (Socket)clientObj;
-        NetworkStream networkStream = new NetworkStream(clientSocket);
+        NetworkStream networkStream = new(clientSocket);
         byte[] message = new byte[4096];
         int bytesRead;
 
         while (true)
         {
-            bytesRead = networkStream.Read(message, 0, message.Length);
-            if (bytesRead == 0)
-                break;
+            try
+            {
+                bytesRead = networkStream.Read(message, 0, message.Length);
+                if (bytesRead == 0)
+                    break;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
 
             string clientMessage = Encoding.ASCII.GetString(message, 0, bytesRead);
             Console.WriteLine($"Received: {clientMessage}");
 
+            List<int> result = _invertedIndex.Search(clientMessage);
+
             byte[] sendBytes = Encoding.ASCII.GetBytes("Server received your message.");
             networkStream.Write(sendBytes, 0, sendBytes.Length);
-            networkStream.Flush();
+            // networkStream.Flush();
+
+            Console.WriteLine($"Documents containing '{clientMessage}':");
+            foreach (var docId in result)
+            {
+                sendBytes = Encoding.ASCII.GetBytes($"Doc #{docId}");
+                networkStream.Write(sendBytes, 0, sendBytes.Length);
+            }
         }
 
         clientSocket.Close();
