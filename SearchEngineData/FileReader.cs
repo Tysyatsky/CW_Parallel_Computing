4d9ea054e0cf9e6039bbe95dcc121a1c57e2182a
Nataliia Tyshchenko <tysyatsky@gmail.com>
2023-12-14 23:46:41 +0200

    fix: moved files to new Project and add thread safe adding files

---
+++ SearchEngineData/FileReader.cs
@@ -0,0 +1,15 @@
+﻿namespace SearchEngineData;
+
+public static class FileReader
+{
+    private static readonly char[] textExtras = { ' ', '\t', '\n', '\r', '.', ',', ';', ':', '!', '?', '>', '<', '"', '/', '(', ')' };
+
+    public static string[] ReadWordsFromFile(string path)
+    {
+        string text = File.ReadAllText(path);
+        return TextToWordsConvertor(text);
+    }
+
+    private static string[] TextToWordsConvertor(string text) =>
+        text.Split(textExtras, StringSplitOptions.RemoveEmptyEntries);
+}
---
+++ SearchEngineData/InvertedIndex.cs
@@ -0,0 +1,59 @@
+﻿namespace SearchEngineData;
+
+public class InvertedIndex
+{
+    private readonly Dictionary<string, List<int>> index;
+    private readonly object _lockObject = new();
+
+    public InvertedIndex()
+    {
+        index = new Dictionary<string, List<int>>();
+    }
+
+    public void AddDocument(int documentId, string[] terms)
+    {
+        lock (_lockObject)
+        {
+            foreach (var term in terms)
+            {
+                if (!index.ContainsKey(term))
+                {
+                    index[term] = new List<int>();
+                }
+
+                if (!index[term].Contains(documentId))
+                {
+                    index[term].Add(documentId);
+                }
+            }
+        }
+    }
+
+    public List<int> Search(string term)
+    {
+        lock(_lockObject)
+        {
+            if (index.ContainsKey(term))
+            {
+                return index[term];
+            }
+            else
+            {
+                return new List<int>();
+            }
+        }
+    }
+
+    public void PrintIndex() // only for testing purposes
+    {
+        foreach (var term in index.Keys)
+        {
+            Console.Write($"{term}: ");
+            foreach (var docId in index[term])
+            {
+                Console.Write($"{docId} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
