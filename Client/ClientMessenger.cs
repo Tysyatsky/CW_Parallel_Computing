a8e1fb5a489ae49225013b1c1c2f768bc1fd4f74
Nataliia Tyshchenko <ntyshchenko@stratadecision.com>
2023-12-24 19:29:17 +0200

    (feature) Adding docs with clearing the project

---
+++ Client/ClientMessenger.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+public static class ClientMessanger
+{
+    private const int ByteLenght = 4096;
+    public static void SendMessage(Socket socket, string message)
+    {
+        var messageBytes = Encoding.ASCII.GetBytes(message);
+        socket.Send(messageBytes);
+    }
+
+    public static string ReceiveMessage(Socket socket)
+    {
+        var receivedBytes = new byte[ByteLenght];
+        var bytesRead = socket.Receive(receivedBytes);
+        return Encoding.ASCII.GetString(receivedBytes, 0, bytesRead);
+    }
+}
